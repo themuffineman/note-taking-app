c5e3aaccde044b6f0050583de3d4a4ea4fbfb522
Petrus <petrusheya@gmail.com>
2023-12-25 19:48:48 +0200

    Added databse connection

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
 const noteText = document.querySelector('input[type="text"]');
 const noteDate = document.querySelector('input[type="date"]');
-const addNewbtn = document.querySelector('button[type="submit"]');
-const notesDiv = document.querySelector('.notes-div');
-const dateDiv = document.querySelector('due-date');
-let noteArray=[];
+
 let dateInput;
 let noteInput;
-let noteID = 0;
 
-class Note {
-    constructor(notetext, duedate, id) {
-      this.text = notetext;
-      this.due = duedate;
-      this.id = id
-    }
-}
-
-noteText.addEventListener('input', function() {
+noteText.addEventListener('input', ()=>{
     noteInput = noteText.value;
-    console.log(noteInput)
+    console.log(noteInput);
 })
-noteDate.addEventListener('input', function() {
-    dateInput = noteDate.value;
+noteDate.addEventListener('input', ()=>{
+    dateInput = noteDate.value
     console.log(dateInput)
 })
 
-function addNewNote(){
-    noteID++
-    noteArray.push(new Note(noteInput, dateInput, noteID));
-    let lastItem = noteArray[noteArray.length - 1];
-    let newNoteHTML = `<div class="notes-and-checkbox" id="${noteID}"> <div class="note"> <p class="to-do"> <strong>To do</strong>: ${lastItem.text}</p><p class="due-date"><strong>Due by</strong>: ${lastItem.due}</p></div><div class="checkbox"><input type="checkbox" onclick='deleteNote(this)'></div></div>`
-    notesDiv.insertAdjacentHTML("beforeend", newNoteHTML);
-    console.log(noteArray);
-    console.log(noteID);
-}
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://sheyapetrus:VMC0kGtSmr48aONo@todocluster.a28gapt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-function deleteNote(checkbox){
-        const parentElement = checkbox.closest('.notes-and-checkbox');
-        let ID = parseInt(parentElement.id, 10) - 1;
-        noteArray.splice(ID, 1);
-        console.log(noteArray);
-        parentElement.remove();
-}
+
+
+const documentToInsert = {
+    task: noteInput,
+    dueDate: new Date(dateInput),
+  };
+
+async function insertDocument(){
+
+    try {
+        await client.connect();
+        
+        const database = client.db("to-do-database");
+
+        const collection = database.collection("to-do-list");
+
+        const result = await collection.insertOne(documentToInsert);
+
+        console.log(`Inserted document with _id: ${result.insertedId}`)
+    } catch{
+        console.error(error)
+    } finally {
+        await client.close();
+      }
+    }
+
+
